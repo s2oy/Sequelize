7391ebe72e005fd74016ed0569dd847870e44aa7
백서연 <w2032@e-mirim.hs.kr>
2021-12-10 00:55:59 +0900

    Add apply body-parser module

--- my-blog/server/server.js
+++ my-blog/server/server.js
@@ -4,9 +4,25 @@ const app = express();
 
 //sequelize 연결
 const sequelize = require('./models').sequelize;
+//body-parser 모듈 적용
+const bodyParser = require('body-parser')
+
 sequelize.sync();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+const {
+  Teacher,
+  Sequelize: { Op }
+} = require('./models');
+sequelize.query('SET NAMES utf8');
+
+// client가 '/app/data'의 주소로 보내는 모든 데이터는 server로 전송됨
+app.post('/app/data', (req,res) => {
+  console.log(req.body)
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
