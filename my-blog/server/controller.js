2a83e9931bf3d72d11bb382312fe363b0e07740f
백서연 <w2032@e-mirim.hs.kr>
2021-12-11 00:04:56 +0900

    Add connet controller to route

--- my-blog/server/controller.js
+++ my-blog/server/controller.js
@@ -5,3 +5,23 @@ const AWS = require('aws-sdk');
 AWS.config.loadFromPath(
 	path.join(__dirname, 'config', 'awsConfig.json')
 );
+
+// 모듈화(객체형태)하고 외부에서 접근할 수  있게 한다.
+// 모델에 접근할 수  있는 경로를 키와 밸류로 설정 가능함
+module.exports = {
+	need: () => load,
+	api: {
+		getData: (req, res) => {
+			console.log('컨트롤러 연결 성공!')
+		} ,
+		getData: (req, res) => {
+		
+		},
+		modifyData: (req, res) => {
+		
+		},
+		deleteData: (req, res) => {
+		
+		},
+	}
+}
--- my-blog/server/route.js
+++ my-blog/server/route.js
@@ -1,6 +1,10 @@
 //라우터는 클라이언트가 서버로 요청할 수 있는 API 주소를 나타냄
 const express = require('express');
 const router = express.Router(); //router 변수를 통해 API주소 연결
+const controller = require('./controller');	//controller 연결
+
+//client는 /get/data를 통해 controller의 api.getData에 접근가능
+router.get('/get/data', controller.api.getData);
 
 //app.get => router.get
 router.get('/get/data');
